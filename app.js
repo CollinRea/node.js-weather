699722fa300e2037bd4c82d2f6d45d7e0596caec
Collin Rea <collinr01@gmail.com>
2017-01-07 21:42:03 -0600

    Adds yargs to take arguments -help and -address

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('h', 'help')
+  .argv;
+
+console.log(argv);
 
 request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lumbard%20st%20philadelphia',
